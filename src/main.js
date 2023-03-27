0b00b406465ded30dc0838a16c4a31ad75b511d9
Bartłomiej Radoń <kuzniar.bartlomiej20@gmail.com>
2023-03-27 07:11:42 +0000

    Remove unused dependency

--- src/main.js
+++ src/main.js
@@ -5,14 +5,10 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './App.vue';
 import router from './router';
 
-import vueAwesomeSidebar from 'vue-awesome-sidebar';
-import 'vue-awesome-sidebar/dist/vue-awesome-sidebar.css';
-
 import './assets/styles.scss';
 
 const app = createApp(App);
 
-app.use(vueAwesomeSidebar);
 app.use(createPinia().use(piniaPluginPersistedstate));
 app.use(router);
 
