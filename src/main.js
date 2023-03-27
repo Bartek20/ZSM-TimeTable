bffaa5ac7b32fab6ef23ff9fc37c984b8e901ee8
Bartłomiej Radoń <kuzniar.bartlomiej20@gmail.com>
2023-03-27 20:09:28 +0000

    SW Update

--- src/main.js
+++ src/main.js
@@ -9,22 +9,13 @@ import './assets/styles.scss';
 
 import { registerSW } from 'virtual:pwa-register';
 
-const intervalMS = 60 * 1000;
-
 const updateSW = registerSW({
 	immediate: true,
-	onNeedRefresh() {
-		console.log('Update found');
-		window.updatable = true;
-	},
 	onRegisteredSW(workerURL, r) {
-		console.log('SW Registered');
-		console.log(r);
 		r &&
 			setInterval(() => {
-				console.log('Updating...');
 				r.update();
-			}, intervalMS);
+			}, 3600000);
 	},
 });
 
