d4c08b7611caf0430afff88c5b91b8c3575cfc9f
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2023-04-05 18:55:20 +0000

    User Var for path

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
 
-const root = '/';
-console.log(process.env);
+const root = process.env.ROOT_PATH || '/';
+console.log('Building for:', process.env.ROOT_PATH);
 
 // https://vitejs.dev/config/
 export default defineConfig({
