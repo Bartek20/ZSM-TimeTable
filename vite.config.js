201967fb6315a9bcbb863cacf4e240abc1c41c35
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2023-04-05 18:51:45 +0000

    Check .ENV Access on CF Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
 
 const root = '/';
+console.log(process.env);
 
 // https://vitejs.dev/config/
 export default defineConfig({
