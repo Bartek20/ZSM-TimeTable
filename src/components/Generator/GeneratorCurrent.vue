7180aaec6fb6195fc04ecafe37841e0f51b3ec6d
Bartłomiej Radoń <b.radon@praktyki.ideo.pl>
2023-05-31 21:38:14 +0200

    Remove old timetables

--- src/components/Generator/GeneratorCurrent.vue
+++ src/components/Generator/GeneratorCurrent.vue
@@ -11,7 +11,7 @@ const subjects = ref({});
 function loadList() {
   return new Promise(async (resolve) => {
     var out = [];
-    const URL = `${location.host != 'zsm.resman.pl' ? import.meta.env.BASE_URL : '/'}plan_nauczyciele/lista.html`;
+    const URL = '/plan_nauczyciele/lista.html';
     const res = await axios.get(URL);
     const lists = new TimeTableList(res.data).getList();
     classes.value = lists.classes;
@@ -27,7 +27,7 @@ function loadPlans() {
       subjects: [],
     };
     for (let i = 1; i <= classes.value.length; i++) {
-      const URL = `${location.host != 'zsm.resman.pl' ? import.meta.env.BASE_URL : '/'}plan_nauczyciele/plany/o${i}.html`;
+      const URL = `/plan_nauczyciele/plany/o${i}.html`;
       const res = await axios.get(URL);
       const TT = new TimeTable(res.data);
       TT.getTitle()
@@ -62,5 +62,4 @@ progress.value = '';
   <section id="generator" class="z-0 w-100 h-100"></section>
 </template>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
--- src/stores/plans.js
+++ src/stores/plans.js
@@ -16,7 +16,7 @@ export const usePlansStore = defineStore('plans', {
   actions: {
     async loadList(force = false) {
       if (!force && this.lists.classes.length != 0) return;
-      const URL = `${location.host != 'zsm.resman.pl' ? import.meta.env.BASE_URL : '/'}plan_nauczyciele/lista.html`;
+      const URL = '/plan_nauczyciele/lista.html';
       var res;
       try {
         res = await axios.get(URL);
@@ -34,9 +34,7 @@ export const usePlansStore = defineStore('plans', {
       this.plans[mode][id] = {
         status: 0,
       };
-      const URL = `${
-        location.host != 'zsm.resman.pl' ? import.meta.env.BASE_URL : '/'
-      }plan_nauczyciele/plany/${mode}${id}.html`;
+      const URL = `/plan_nauczyciele/plany/${mode}${id}.html`;
       var res;
       try {
         res = await axios.get(URL);
@@ -80,19 +78,13 @@ export const usePlansStore = defineStore('plans', {
     },
     async getPlans() {
       this.lists.classes.forEach((obj) => {
-        axios.get(
-          `${location.host != 'zsm.resman.pl' ? import.meta.env.BASE_URL : '/'}plan_nauczyciele/plany/o${obj.value}.html`
-        );
+        axios.get(`/plan_nauczyciele/plany/o${obj.value}.html`);
       });
       this.lists.teachers.forEach((obj) => {
-        axios.get(
-          `${location.host != 'zsm.resman.pl' ? import.meta.env.BASE_URL : '/'}plan_nauczyciele/plany/n${obj.value}.html`
-        );
+        axios.get(`/plan_nauczyciele/plany/n${obj.value}.html`);
       });
       this.lists.rooms.forEach((obj) => {
-        axios.get(
-          `${location.host != 'zsm.resman.pl' ? import.meta.env.BASE_URL : '/'}plan_nauczyciele/plany/s${obj.value}.html`
-        );
+        axios.get(`/plan_nauczyciele/plany/s${obj.value}.html`);
       });
     },
     async getTimeTable() {
