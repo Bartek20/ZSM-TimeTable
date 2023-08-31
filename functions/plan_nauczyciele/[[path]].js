33a118804839303a370df835b30535fca4ae247a
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2023-08-31 20:19:30 +0200

    Temp path change

---
+++ functions/plan_nauczyciele/[[path]].js
@@ -0,0 +1,10 @@
+async function getFile(url) {
+  const res = await fetch(url);
+  const resp = new Response(res.body, res);
+  resp.headers.set('Access-Control-Allow-Origin', 'https://zsm-timetable.pages.dev');
+  return resp;
+}
+
+export async function onRequest(context) {
+  return await getFile(`https://zsm.resman.pl/plan_nauczyciele/${context.params.path.join('/')}`);
+}
