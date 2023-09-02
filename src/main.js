96536e750c7ff38603a8039b53ef39a4a72da5f9
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2023-09-02 11:50:08 +0200

    Update caching & Add old timetable redirect

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import router from './router';
 
 import './assets/styles.scss';
 
+async function cacheTimeTables() {
+	if (import.meta.env.MODE == 'development') return;
+	const last = window.localStorage.getItem('lastFetched');
+	if (last != null && last + 86400000 > Date.now()) return;
+	console.info('Fetching timetable updates started.');
+	axios.get(`${import.meta.env.BASE_URL}school-data.json`);
+	const res = await axios.get('/plan_nauczyciele/lista.html');
+	const list = new TimeTableList(res.data).getList();
+	const classMap = list.classes.map((obj) => axios.get(`/plan_nauczyciele/plany/o${obj.value}.html`));
+	const teacherMap = list.teachers.map((obj) => axios.get(`/plan_nauczyciele/plany/n${obj.value}.html`));
+	const roomMap = list.rooms.map((obj) => axios.get(`/plan_nauczyciele/plany/s${obj.value}.html`));
+	await Promise.all([classMap, teacherMap, roomMap]);
+	console.info('Fetching timetable updates finished.');
+	window.localStorage.setItem('lastFetched', Date.now());
+}
+
 function waitForWorker(SW) {
 	return new Promise((resolve) => {
 		const checkStatus = () => {
@@ -17,7 +33,7 @@ const updateSW = registerSW({
 	immediate: true,
 	async onRegisteredSW(_, SW) {
 		await waitForWorker(SW);
-		window.WorkerReady = true;
+		if (!location.pathname.endsWith('/print')) cacheTimeTables();
 		setInterval(() => {
 			SW.update();
 		}, 3600000);
--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -1,33 +1,6 @@
 <script setup>
 	const mode = useRouteParams('mode');
 	const id = useRouteParams('id');
-	async function load() {
-		const last = window.localStorage.getItem('lastFetched');
-		if (import.meta.env.MODE == 'development') return;
-		if (last != null && last + 86400000 > Date.now()) return;
-		console.log('Waiting for SW before fetching...');
-		var i = 0;
-		while (window.WorkerReady == undefined && i < 600) {
-			await sleep(100);
-			i++;
-		}
-		await sleep(3000);
-		console.log('Fetching timetable updates...');
-		axios.get(`${import.meta.env.BASE_URL}school-data.json`);
-		const res = await axios.get('/plan_nauczyciele/lista.html');
-		const list = new TimeTableList(res.data).getList();
-		list.classes.forEach((obj) => {
-			axios.get(`/plan_nauczyciele/plany/o${obj.value}.html`);
-		});
-		list.teachers.forEach((obj) => {
-			axios.get(`/plan_nauczyciele/plany/n${obj.value}.html`);
-		});
-		list.rooms.forEach((obj) => {
-			axios.get(`/plan_nauczyciele/plany/s${obj.value}.html`);
-		});
-		window.localStorage.setItem('lastFetched', Date.now());
-	}
-	load();
 </script>
 
 <template>
