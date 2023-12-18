10b241f20349f32d2578b372db06f9acb05fb50c
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2023-12-18 22:25:17 +0100

    Fix

--- functions/plan_vulcan/[[path]].js
+++ functions/plan_vulcan/[[path]].js
@@ -1,10 +1,10 @@
 async function getFile(url) {
-  const res = await fetch(url);
-  const resp = new Response(res.body, res);
-  resp.headers.set('Access-Control-Allow-Origin', 'https://zsm-timetable.pages.dev');
-  return resp;
+	const res = await fetch(url);
+	const resp = new Response(res.body, res);
+	resp.headers.set('Access-Control-Allow-Origin', 'https://zsm-timetable.pages.dev');
+	return resp;
 }
 
 export async function onRequest(context) {
-  return await getFile(`https://zsm.resman.pl/plan_vulcan/${context.params.path.join('/')}`);
+	return await getFile(`https://zsm.resman.pl/plan_vulcan/${context.params.path.join('/')}?app=timetable`);
 }
