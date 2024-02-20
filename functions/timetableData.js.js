bd7989920d9b7bc26bb2d33de2014927878fcc98
Bartek20 <kuzniar.bartlomiej20@gmail.com>
2024-02-20 19:53:56 +0100

    Functions fix

--- functions/timetableData.js.js
+++ functions/timetableData.js.js
@@ -1,10 +1,10 @@
-async function getFile(url) {
+async function getFile(domain, url) {
 	const res = await fetch(url);
 	const resp = new Response(res.body, res);
-	resp.headers.set('Access-Control-Allow-Origin', env.CF_PAGES_URL);
+	resp.headers.set('Access-Control-Allow-Origin', domain);
 	return resp;
 }
 
-export async function onRequest(_context) {
-	return await getFile(`https://zsm.resman.pl/plan_lekcji/timetableData.js`);
+export async function onRequest(context) {
+	return await getFile(context.env.CF_PAGES_URL, `https://zsm.resman.pl/plan_lekcji/timetableData.js`);
 }
