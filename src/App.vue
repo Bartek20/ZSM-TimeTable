642545f8e5e761fb875babac72112ddd90d739d4
Bartek20 <kuzniar.bartlomiej20@gmail.com>
2024-02-25 23:41:09 +0100

    PWA Code

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup>
+	const PWAStore = usePWAStore();
+	window.addEventListener('beforeinstallprompt', (e) => {
+		e.preventDefault();
+		PWAStore.event = e;
+		PWAStore.installed = false;
+	});
+</script>
 
 <template>
 	<RouterView />
