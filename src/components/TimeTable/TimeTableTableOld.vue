2d0712b5f9eac691f23105c5b98357e919639eae
Bartek20 <kuzniar.bartlomiej20@gmail.com>
2024-03-11 09:42:57 +0100

    Remove old code

--- src/components/TimeTable/TimeTableTableOld.vue
+++ src/components/TimeTable/TimeTableTableOld.vue
@@ -1,5 +1,4 @@
 <script setup lang="js">
-	import appData from '@/stores/data';
 	import appConfigs from '@/stores/configs';
 	 const props = defineProps({
 		data: {
@@ -7,54 +6,6 @@
 			required: true,
 		},
 	});
-
-	function addTime(clock, time) {
-		const padClock = (part) => (part < 10 ? '0' + part : part);
-		if (!clock || !time) return undefined;
-		const [hour, minute] = clock.split(':');
-		const date = new Date();
-		date.setHours(hour, minute, 0, 0);
-		date.setTime(date.getTime() + time * 60000);
-		return padClock(date.getHours()) + ':' + padClock(date.getMinutes());
-	}
-	function checkBetween(from, to) {
-		if (!from || !to) return false;
-		const current = new Date();
-		const sTime = new Date();
-		const eTime = new Date();
-		const startTime = from.split(':');
-		const endTime = to.split(':');
-		sTime.setHours(startTime[0], startTime[1], 0, 0);
-		eTime.setHours(endTime[0], endTime[1] - 1, 59, 999);
-		return current >= sTime && current <= eTime;
-	}
-
-
-
-	const TIME = useDateFormat(useNow({ interval: 100 }), 'd;HH:mm', {
-		locales: 'pl-PL',
-	});
-	const currentDay = ref(-1);
-	const currentLesson = ref(-1);
-	watch(
-		[TIME, () => props.data],
-		() => {
-			const timeData = TIME.value.split(';');
-			currentDay.value = parseInt(timeData[0]) - 1;
-			currentLesson.value = -1;
-			props.data.forEach((row) => {
-				if (checkBetween(row.hours.from, row.hours.to)) {
-					currentLesson.value = row.nr;
-					return;
-				}
-				if (checkBetween(row.hours.to, addTime(row.hours.to, row.break))) {
-					currentLesson.value = row.nr + 'break';
-					return;
-				}
-			});
-		},
-		{ immediate: true }
-	);
 </script>
 
 <template>
@@ -74,12 +25,12 @@
 					<th class="nrOld">{{ row.nr }}</th>
 					<td class="timeOld">{{ row.hours.from }}-{{ row.hours.to }}</td>
 					<td class="lessonOld" v-for="(day, i) in row.lessons">
-						<div :class="{ current: appConfigs.showCurrent && day.length > 0 && currentLesson == row.nr && currentDay == i }">
+						<div>
 							<TimeTableLessonOld v-for="lesson in day" :data="lesson" />
 						</div>
 					</td>
 				</tr>
-				<tr v-if="appConfigs.showBreaks && row.break != 0" :class="{ current: appConfigs.showCurrent && currentLesson == row.nr + 'break' }">
+				<tr v-if="appConfigs.showBreaks && row.break != 0">
 					<th></th>
 					<td colspan="6" class="timetable__old__break lessonOld">
 						{{ `Przerwa ${row.break}-minutowa` }}
