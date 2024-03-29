4e3dc3b37b77adc445ec4839656102021255062b
Bartek20 <kuzniar.bartlomiej20@gmail.com>
2024-03-29 23:11:03 +0100

    Fix

--- src/main.js
+++ src/main.js
@@ -220,6 +220,7 @@ const updateSW = registerSW({
 			checkStatus();
 		});
 		SW.active.addEventListener('message', async event => {
+			if (fetching) return
 			if (event.data.meta !== 'workbox-broadcast-update') return
 			const { cacheName } = event.data.payload;
 			if (cacheName !== 'timetables-data') return
