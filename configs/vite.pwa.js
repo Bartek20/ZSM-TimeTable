a8418ffdd9045a45457ada0911e3b5d50b48222c
Bartek20 <kuzniar.bartlomiej20@gmail.com>
2024-03-30 23:55:07 +0100

    Logging

--- configs/vite.pwa.js
+++ configs/vite.pwa.js
@@ -47,9 +47,11 @@ const updateBroadcaster = {
 		} else if (file.match(/[ons]{1}\d+\.html/)) {
 			const genOld = /wygenerowano (\d{1,4}[./-]\d{1,2}[./-]\d{1,4})/.exec(oldText)
 			const genNew = /wygenerowano (\d{1,4}[./-]\d{1,2}[./-]\d{1,4})/.exec(newText)
+			console.log(genOld, genNew)
 			if (genOld?.[1] != genNew?.[1]) updated = true
 			const applyOld = /^Obowiązuje od: (.+)$/.exec(oldText)
 			const applyNew = /^Obowiązuje od: (.+)$/.exec(newText)
+			console.log(applyOld, applyNew)
 			if (applyOld?.[1] != applyNew?.[1]) updated = true
 		}
 		if (!updated) return
