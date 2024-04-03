f23368a8b00a2b014884110b46a9570853df5b22
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2024-04-03 20:56:48 +0200

    Add toasts (#104)
    * Add toasts
    * Fix installLatest bug
    * App update detection
    * Fix Sentry attachment bug

--- configs/vite.server.js
+++ configs/vite.server.js
@@ -1,37 +1,44 @@
 const proxyConfigure = (proxy, _options) => {
-	const headings = {
-		ERROR: '[PROXY] [ERROR]   ',
-		REQUEST: '[PROXY] [REQUEST] ',
-		RESPONSE: '[PROXY] [RESPONSE]',
-	};
-	proxy.on('error', (err, _req, _res) => {
-		console.log(headings.ERROR, err);
-	});
-	proxy.on('proxyReq', (_proxyReq, req, _res) => {
-		const method = '[' + req.method + ']'.padEnd(5, ' ');
-		console.log(headings.REQUEST, method, req.url);
-	});
-	proxy.on('proxyRes', (proxyRes, req, _res) => {
-		const code = '[' + proxyRes.statusCode + ']'.padEnd(5, ' ');
-		console.log(headings.RESPONSE, code, req.url);
-	});
-};
-
-async function getProxy() {
-	const proxy = {};
-	const schoolData = await import('../public/schoolData').then(e => e.default).catch(() => ({}));
-	if (!schoolData.schoolTimeTableRootURL) return proxy
-	console.log('Setting up PROXY for:', schoolData.schoolTimeTableRootURL);
-	proxy[ schoolData.schoolTimeTableRootURL ] = {
-		target: schoolData.schoolHomeURL,
-		changeOrigin: true,
-		secure: false,
-		ws: true,
-		configure: proxyConfigure,
-	}
-	return proxy;
+  const headings = {
+    ERROR: '[PROXY] [ERROR]   ',
+    REQUEST: '[PROXY] [REQUEST] ',
+    RESPONSE: '[PROXY] [RESPONSE]'
+  }
+  proxy.on('error', (err, _req, _res) => {
+    console.log(headings.ERROR, err)
+  })
+  proxy.on('proxyReq', (_proxyReq, req, _res) => {
+    const method = `[${req.method}]`.padEnd(5, ' ')
+    console.log(headings.REQUEST, method, req.url)
+  })
+  proxy.on('proxyRes', (proxyRes, req, _res) => {
+    const code = `[${proxyRes.statusCode}]`.padEnd(5, ' ')
+    console.log(headings.RESPONSE, code, req.url)
+  })
 }
 
 export default {
-	proxy: await getProxy(),
-};
+  proxy: {
+    '/plan_vulcan': {
+      target: 'https://zsm.resman.pl',
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+      configure: proxyConfigure
+    },
+    '/timetableData.js': {
+      target: 'https://zsm.resman.pl/plan_lekcji',
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+      configure: proxyConfigure
+    },
+    '/schoolData.js': {
+      target: 'https://zsm.resman.pl/plan_lekcji',
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+      configure: proxyConfigure
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,62 +1,77 @@
 // Floating tooltip css
-import 'floating-vue/dist/style.css';
+import 'floating-vue/dist/style.css'
+// Toast css
+import '../node_modules/vue-toastification/dist/index.css'
 // App css
-import './assets/main.scss';
+import './assets/main.scss'
 
-import { createApp } from 'vue';
-import { vTooltip } from 'floating-vue';
+import { createApp } from 'vue'
+import { vTooltip } from 'floating-vue'
+import Toast, { useToast } from 'vue-toastification'
 
-import App from './App.vue';
-import router from './router';
+import App from './App.vue'
+import router from './router'
 
-import appConfigs from '@/stores/configs';
-import appPWA from '@/stores/pwa';
-import log from '@/functions/logger';
-import validateApp from '@/functions/appVersionControl';
-import colorHandler from '@/functions/colorModeHandler';
-import parseData from '@/functions/timetableDataHandler';
+import appConfigs from '@/stores/configs'
+import appPWA from '@/stores/pwa'
+import log from '@/functions/logger'
+import validateApp from '@/functions/appVersionControl'
+import colorHandler from '@/functions/colorModeHandler'
+import parseData from '@/functions/timetableDataHandler'
 
-import { init as Sentry_init, browserTracingIntegration as Sentry_browserTracingIntegration, replayIntegration as Sentry_replayIntegration } from '@sentry/vue';
+import {
+  init as Sentry_init,
+  browserTracingIntegration as Sentry_browserTracingIntegration,
+  replayIntegration as Sentry_replayIntegration
+} from '@sentry/vue'
 
-const app = createApp(App);
+const app = createApp(App)
 
 // Sentry Error Reporting
-if (__SENTRY_DSN__) Sentry_init({
-	// Sentry config
-	dsn: __SENTRY_DSN__,
-	integrations: [
-		Sentry_browserTracingIntegration({ router }),
-		Sentry_replayIntegration({
-			maskAllText: false,
-			blockAllMedia: false,
-		}),
-	],
-	// Performance Monitoring
-	tracesSampleRate: 0.1,
-	tracePropagationTargets: [ /^(?!.*cloudflareinsights\.com).*/ ],
-	// Session Replay
-	replaysSessionSampleRate: 0.0,
-	replaysOnErrorSampleRate: 1.0,
-	// Vue settings
-	app,
-	trackComponents: true,
-	// App settings
-	initialScope: (scope) => {
-		const data = JSON.parse(localStorage.getItem('appConfigs'));
-		scope.setTag('appVersion', __APP_VERSION__);
-		scope.setContext('appConfigs', data);
-		return scope;
-	},
-	beforeSend: (event, hint) => {
-		hint.attachments = [ { filename: 'appConfigs.json', data: () => localStorage.getItem('appConfigs') } ];
-		return event;
-	},
-});
+if (__SENTRY_DSN__) {
+  Sentry_init({
+    // Sentry config
+    dsn: __SENTRY_DSN__,
+    integrations: [
+      Sentry_browserTracingIntegration({ router }),
+      Sentry_replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false
+      })
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 0.1,
+    tracePropagationTargets: [/^(?!.*cloudflareinsights\.com).*/],
+    // Session Replay
+    replaysSessionSampleRate: 0.0,
+    replaysOnErrorSampleRate: 1.0,
+    // Vue settings
+    app,
+    trackComponents: true,
+    // App settings
+    initialScope: (scope) => {
+      const data = JSON.parse(localStorage.getItem('appConfigs'))
+      scope.setTag('appVersion', __APP_VERSION__)
+      scope.setContext('appConfigs', data)
+      return scope
+    },
+    beforeSend: (event, hint) => {
+      const confs = JSON.stringify(
+        JSON.parse(localStorage.getItem('appConfigs') ?? '{}'),
+        null,
+        '\t'
+      )
+      if (confs === '{}') return event
+      hint.attachments = [{ filename: 'appConfigs.json', data: confs }]
+      return event
+    }
+  })
+}
 
 // Check custom css scrollbar support
-function checkScrollStyllability() {
-	const style = document.createElement('style');
-	style.textContent = `
+function checkScrollStyllability () {
+  const style = document.createElement('style')
+  style.textContent = `
 	.scrolltest {
 		position: fixed;
 		visibility: hidden;
@@ -76,166 +91,254 @@ function checkScrollStyllability() {
 	}
 	.scrolltest::-webkit-scrollbar-thumb {
 		background: green;
-	}`;
-	const test = document.createElement('div');
-	test.classList.add('scrolltest');
-	document.head.appendChild(style);
-	document.body.appendChild(test);
-	if (test.clientWidth !== 97) document.body.classList.add('notstyllablescroll');
-	if (test.clientWidth !== 97 && test.clientWidth !== 100) document.body.classList.add('thinscroll');
-	log(
-		'info',
-		'[App] Test zakończony z wynikiem:\nCustomowy scrollbar: ',
-		test.clientWidth !== 97 ? (test.clientWidth !== 100 ? 'Nie wspierany (Aktywny cienki scrollbar)' : 'Nie wspierany (Aktywny normalny scrollbar)') : 'Wspierany'
-	);
-	style.remove();
-	test.remove();
+	}`
+  const test = document.createElement('div')
+  test.classList.add('scrolltest')
+  document.head.appendChild(style)
+  document.body.appendChild(test)
+  if (test.clientWidth !== 97) {
+    document.body.classList.add('notstyllablescroll')
+  }
+  if (test.clientWidth !== 97 && test.clientWidth !== 100) {
+    document.body.classList.add('thinscroll')
+  }
+  log(
+    'info',
+    '[App] Test zakończony z wynikiem:\nCustomowy scrollbar: ',
+    test.clientWidth !== 97
+      ? test.clientWidth !== 100
+        ? 'Nie wspierany (Aktywny cienki scrollbar)'
+        : 'Nie wspierany (Aktywny normalny scrollbar)'
+      : 'Wspierany'
+  )
+  style.remove()
+  test.remove()
 }
 
 // Set PWA eventlistener
 window.addEventListener('beforeinstallprompt', (e) => {
-	e.preventDefault();
-	appPWA.event.value = e;
-	appPWA.installed.value = false;
-});
-window.addEventListener('appinstalled', () => {
-	appPWA.event.value = undefined;
-	appPWA.installed.value = true;
+  e.preventDefault()
+  appPWA.event.value = e
+  appPWA.installed.value = false
 })
-window.removeEventListener('beforeinstallprompt', window.installhandler);
+window.addEventListener('appinstalled', () => {
+  appPWA.event.value = undefined
+  appPWA.installed.value = true
+})
+window.removeEventListener('beforeinstallprompt', window.installhandler)
 if (window.installevent) {
-	appPWA.event.value = window.installevent;
-	appPWA.installed.value = false;
-	window.installhandler = undefined;
-	window.installevent = undefined;
+  appPWA.event.value = window.installevent
+  appPWA.installed.value = false
+  window.installhandler = undefined
+  window.installevent = undefined
 }
 
 // Service Worker, Timetable caching
 let fetching = false
-async function cacheTimeTables() {
-	if (!appConfigs.value.school.timetableURL) return;
-	if (import.meta.env.MODE === 'development') return;
-	if (fetching) return
-	fetching = true
-	const last = appConfigs.value.lastFetched;
-	if (last != null && last + 86400000 > Date.now()) return;
-	log('info', '[Service Worker] Rozpoczęto pobieranie planów do pamięci cache.');
-	try {
-		const res = await axios.get(`${appConfigs.value.school.timetableURL}lista.html`);
-		const list = new TimeTableList(res.data).getList();
-		const classMap = list.classes.map((obj) => axios.get(`${appConfigs.value.school.timetableURL}plany/o${obj.value}.html`).catch((_) => { }));
-		const teacherMap = list.teachers.map((obj) => axios.get(`${appConfigs.value.school.timetableURL}plany/n${obj.value}.html`).catch((_) => { }));
-		const roomMap = list.rooms.map((obj) => axios.get(`${appConfigs.value.school.timetableURL}plany/s${obj.value}.html`).catch((_) => { }));
-		await Promise.all([ ...classMap, ...teacherMap, ...roomMap ]);
-		log('info', '[Service Worker] Zakończono pobieranie planów do pamięci cache.');
-		appConfigs.value.lastFetched = Date.now();
-		fetching = false
-	} catch (e) {
-		log('error', '[Service Worker] Wystąpił błąd przy zapisywaniu planów do pamięci cache:\n', e);
-		fetching = false
-	}
+async function cacheTimeTables () {
+  if (!appConfigs.value.school.timetableURL) return
+  if (import.meta.env.MODE === 'development') return
+  if (fetching) return
+  fetching = true
+  const last = appConfigs.value.lastFetched
+  if (last != null && last + 86400000 > Date.now()) return
+  log(
+    'info',
+    '[Service Worker] Rozpoczęto pobieranie planów do pamięci cache.'
+  )
+  try {
+    const res = await axios.get(
+      `${appConfigs.value.school.timetableURL}lista.html`
+    )
+    const list = new TimeTableList(res.data).getList()
+    const classMap = list.classes.map((obj) =>
+      axios
+        .get(`${appConfigs.value.school.timetableURL}plany/o${obj.value}.html`)
+        .catch(() => undefined)
+    )
+    const teacherMap = list.teachers.map((obj) =>
+      axios
+        .get(`${appConfigs.value.school.timetableURL}plany/n${obj.value}.html`)
+        .catch(() => undefined)
+    )
+    const roomMap = list.rooms.map((obj) =>
+      axios
+        .get(`${appConfigs.value.school.timetableURL}plany/s${obj.value}.html`)
+        .catch(() => undefined)
+    )
+    await Promise.all([...classMap, ...teacherMap, ...roomMap])
+    log(
+      'info',
+      '[Service Worker] Zakończono pobieranie planów do pamięci cache.'
+    )
+    appConfigs.value.lastFetched = Date.now()
+    fetching = false
+  } catch (e) {
+    log(
+      'error',
+      '[Service Worker] Wystąpił błąd przy zapisywaniu planów do pamięci cache:\n',
+      e
+    )
+    fetching = false
+  }
 }
 registerSW({
-	immediate: true,
-	onNeedRefresh() {
-		log('info', '[Service Worker] Aplikacja oczekuje na odświeżenie strony.');
-	},
-	onOfflineReady() {
-		log('info', '[Service Worker] Aplikacja jest gotowa do pracy offline.');
-	},
-	async onRegisteredSW(_, SW) {
-		log('info', '[Service Worker] Zainstalowano Service Workera.');
-		// Waiting for Service Worker to install
-		await new Promise((resolve) => {
-			const checkStatus = () => {
-				if (SW.active) resolve();
-				else setTimeout(checkStatus, 100);
-			};
-			checkStatus();
-		});
-		// Service Worker Installed working
-		log('info', '[Service Worker] Service Worker został aktywowany.');
-		await validateApp();
-		cacheTimeTables();
-		setInterval(() => {
-			SW.update();
-		}, 3600000);
-	},
-	onRegisterError(err) {
-		log('error', '[Service Worker] Wystąpił błąd przy rejestracji Service Workera:\n', err);
-	},
-});
-navigator.serviceWorker.addEventListener('message', async event => {
-	if (fetching) return
-	if (event.data !== 'Timetable Update Available') return
-	log('info', '[Service Worker] Wykryto aktualizację planu lekcji - pobieranie aktualnych planów...')
-	appConfigs.value.lastFetched = null
-	cacheTimeTables()
-});
+  immediate: true,
+  onNeedRefresh () {
+    log('info', '[Service Worker] Aplikacja oczekuje na odświeżenie strony.')
+  },
+  onOfflineReady () {
+    log('info', '[Service Worker] Aplikacja jest gotowa do pracy offline.')
+  },
+  async onRegisteredSW (_, SW) {
+    log('info', '[Service Worker] Zainstalowano Service Workera.')
+    // Waiting for Service Worker to install
+    await new Promise((resolve) => {
+      const checkStatus = () => {
+        if (SW.active) resolve()
+        else setTimeout(checkStatus, 100)
+      }
+      checkStatus()
+    })
+    // Service Worker Installed working
+    log('info', '[Service Worker] Service Worker został aktywowany.')
+    await validateApp()
+    cacheTimeTables()
+    setInterval(() => {
+      SW.update()
+    }, 3600000)
+  },
+  onRegisterError (err) {
+    log(
+      'error',
+      '[Service Worker] Wystąpił błąd przy rejestracji Service Workera:\n',
+      err
+    )
+  }
+})
+navigator.serviceWorker.addEventListener('message', (event) => {
+  if (fetching) return
+  if (event.data !== 'Timetable Update Available') return
+  log(
+    'info',
+    '[Service Worker] Wykryto aktualizację planu lekcji - pobieranie aktualnych planów...'
+  )
+  appConfigs.value.lastFetched = null
+  cacheTimeTables()
+})
+if (navigator.serviceWorker.controller) {
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    window.sessionStorage.setItem('swUpdate', 'true')
+  })
+}
 
 // Main app functions
 (async () => {
-	// Console log app version
-	log('info', '[App] Wczytywanie aplikacji w wersji:', __APP_VERSION__);
-	// Console log user notification
-	if (import.meta.env.MODE !== 'development') {
-		log(
-			'info',
-			'[App] Witaj użytkowniku!\nWidzę, że zainteresowało cię działanie mojej aplikacji.\nJeśli masz jakieś pomysły na udoskonalenie jej zapraszam do kontaktu poprzez wątek na githubie:\nhttps://github.com/Bartek20/ZSM-TimeTable/issues'
-		);
-		log('warn', '[App] Jeśli jednak jesteś tu z innego powodu zalecam wycofanie się i zamknięcie tego okna.\nZ pozdrowieniami autor kodu.');
-	}
+  // Console log app version
+  log('info', '[App] Wczytywanie aplikacji w wersji:', __APP_VERSION__)
+  // Console log user notification
+  if (import.meta.env.MODE !== 'development') {
+    log(
+      'info',
+      '[App] Witaj użytkowniku!\nWidzę, że zainteresowało cię działanie mojej aplikacji.\nJeśli masz jakieś pomysły na udoskonalenie jej zapraszam do kontaktu poprzez wątek na githubie:\nhttps://github.com/Bartek20/ZSM-TimeTable/issues'
+    )
+    log(
+      'warn',
+      '[App] Jeśli jednak jesteś tu z innego powodu zalecam wycofanie się i zamknięcie tego okna.\nZ pozdrowieniami autor kodu.'
+    )
+  }
 
-	// App setup
-	// Check supported scrollbars
-	checkScrollStyllability();
-	// Setup cache headers
-	axios.defaults.headers.get[ 'Cache-Control' ] = 'no-cache';
-	// Setup color mode handler
-	colorHandler();
+  // App setup
+  app.use(Toast, {
+    transition: 'Vue-Toastification__bounce',
+    maxToasts: 10,
+    newestOnTop: true,
+    filterBeforeCreate: (toast, toasts) => {
+      if (toasts.filter((t) => t.content === toast.content).length !== 0) {
+        return false
+      }
+      return toast
+    },
+    position: 'top-right',
+    timeout: 3000,
+    closeOnClick: false,
+    pauseOnFocusLoss: false,
+    pauseOnHover: false,
+    draggable: false,
+    draggablePercent: 0.3,
+    showCloseButtonOnHover: true,
+    hideProgressBar: false,
+    closeButton: 'button',
+    rtl: false
+  })
+  app.directive('tooltip', vTooltip)
+  // Check supported scrollbars
+  checkScrollStyllability()
+  // Setup cache headers
+  axios.defaults.headers.get['Cache-Control'] = 'no-cache'
+  // Setup color mode handler
+  colorHandler()
 
-	// Fetch School Data
-	try {
-		const { default: schoolData } = await import(/* @vite-ignore */ `${import.meta.env.BASE_URL}schoolData.js?t=${Date.now()}`);
-		appConfigs.value.school.homeURL = schoolData.schoolHomeURL;
-		appConfigs.value.school.timetableURL = schoolData.schoolTimeTableRootURL;
-		appConfigs.value.school.logoDescription = schoolData.schoolLogoDescription || 'Logo Szkoły';
-	} catch (e) {
-		log('error', '[App] Wystąpił błąd przy wczytywaniu danych szkoły:\n', e);
-	}
-	// Fetch TimeTable Data
-	try {
-		const { default: timetableData } = await import(/* @vite-ignore */ `${import.meta.env.BASE_URL}timetableData.js?t=${Date.now()}`);
-		parseData('shortLessons', timetableData.shortLessons || []);
-		parseData('levels', timetableData.levels || {});
-		parseData('classes', timetableData.classes || {});
-		parseData('teachers', timetableData.teachers || {});
-		parseData('rooms', timetableData.rooms || {});
-		parseData('subjects', timetableData.subjects || {});
-	} catch (e) {
-		log('error', '[App] Wystąpił błąd przy wczytywaniu danych planu lekcji:\n', e);
-	}
+  const toast = useToast()
+  // Fetch School Data
+  try {
+    const { default: schoolData } = await import(
+      /* @vite-ignore */ `${import.meta.env.BASE_URL}schoolData.js?t=${Date.now()}`
+    )
+    appConfigs.value.school.homeURL = schoolData.schoolHomeURL
+    appConfigs.value.school.timetableURL = schoolData.schoolTimeTableRootURL
+    appConfigs.value.school.logoDescription =
+      schoolData.schoolLogoDescription || 'Logo Szkoły'
+  } catch (e) {
+    log('error', '[App] Wystąpił błąd przy wczytywaniu danych szkoły:\n', e)
+    toast.error('Wystąpił błąd przy wczytywaniu danych szkoły')
+  }
+  // Fetch TimeTable Data
+  try {
+    const { default: timetableData } = await import(
+      /* @vite-ignore */ `${import.meta.env.BASE_URL}timetableData.js?t=${Date.now()}`
+    )
+    parseData('shortLessons', timetableData.shortLessons || [])
+    parseData('levels', timetableData.levels || {})
+    parseData('classes', timetableData.classes || {})
+    parseData('teachers', timetableData.teachers || {})
+    parseData('rooms', timetableData.rooms || {})
+    parseData('subjects', timetableData.subjects || {})
+  } catch (e) {
+    log(
+      'error',
+      '[App] Wystąpił błąd przy wczytywaniu danych planu lekcji:\n',
+      e
+    )
+    toast.error('Wystąpił błąd przy wczytywaniu danych planu lekcji')
+  }
 
-	// Prevent app from running without required data
-	if (!appConfigs.value.school.timetableURL) {
-		const loader = document.body.querySelector('#loader');
-		loader.querySelector('h1').innerText = 'Wystąpił błąd przy wczytywaniu aplikacji';
-		loader.querySelector('p').innerText = 'Nie udało się wczytać wymaganych danych.\nSprawdź połączenie z siecią i spróbuj ponownie później.';
-		return;
-	}
+  // Prevent app from running without required data
+  if (!appConfigs.value.school.timetableURL) {
+    const loader = document.body.querySelector('#loader')
+    loader.querySelector('h1').innerText =
+      'Wystąpił błąd przy wczytywaniu aplikacji'
+    loader.querySelector('p').innerText =
+      'Nie udało się wczytać wymaganych danych.\nSprawdź połączenie z siecią i spróbuj ponownie później.'
+    toast.error('Wystąpił błąd przy wczytywaniu aplikacji')
+    return
+  }
 
-	// Try to lock screen orientation
-	try {
-		await screen.orientation.lock('portrait');
-	} catch (e) {
-		log('warn', '[App] Nie udało się zablokować orientacji ekranu:\n', e);
-	}
+  // Try to lock screen orientation
+  try {
+    await screen.orientation.lock('portrait')
+  } catch (e) {
+    log('warn', '[App] Nie udało się zablokować orientacji ekranu:\n', e)
+  }
 
-	// Reset session configs
-	appConfigs.value.shortLessons = false;
+  // Reset session configs
+  appConfigs.value.shortLessons = false
+  if (window.sessionStorage.getItem('swUpdate') !== null) {
+    window.sessionStorage.removeItem('swUpdate')
+    toast.success('Zaktualizowano aplikację do najnowszej wersji')
+  }
 
-	// Render application
-	app.use(router);
-	app.directive('tooltip', vTooltip);
-	app.mount('#app');
-})();
+  // Render application
+  app.use(router)
+  app.mount('#app')
+})()
