8506fcf0d57965f06698f687e997853eba7a4424
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2024-08-08 22:35:19 +0200

    Simplify return

--- src/functions/recentHistory.js
+++ src/functions/recentHistory.js
@@ -10,25 +10,18 @@ export function addHistory(mode, id) {
   // Change history size to 24 records
   historyRecords = historyRecords.slice(0, 24);
   // Add newest record
-  historyRecords = [{ mode, id }, ...historyRecords];
+  historyRecords = [ { mode, id }, ...historyRecords ];
   // Save history
   appConfigs.value.history = historyRecords;
 }
 
 export function getHistory(mode, count = 5) {
   const history = mode === "nauczyciel"
-  ? appConfigs.value.history
-  : appConfigs.value.history.filter((record) => {
+    ? appConfigs.value.history
+    : appConfigs.value.history.filter((record) => {
       // Exclude record not accessible for user based on school config
-      if (
-        (record.mode === "n" &&
-          !appConfigs.value.school.allowStrudentsViewTeachers) ||
-        (record.mode === "s" &&
-          !appConfigs.value.school.allowStrudentsViewRooms)
-      ) {
-        return false;
-      }
-      return true;
+      return (record.mode === "n" && !appConfigs.value.school.allowStrudentsViewTeachers) ||
+        (record.mode === "s" && !appConfigs.value.school.allowStrudentsViewRooms)
     })
   return history.length <= count ? history : history.slice(0, count);
 }
