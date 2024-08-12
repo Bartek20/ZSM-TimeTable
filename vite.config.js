145daa8bef8a5a47f81bc0599c5e24347fdfa39a
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2024-08-12 22:43:28 +0200

    Get rid of google font cdn

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import webfontDownload from 'vite-plugin-webfont-dl';
 import Components from "unplugin-vue-components/vite";
 import AutoImport from "unplugin-auto-import/vite";
 import autoprefixer from "autoprefixer";
@@ -40,6 +41,13 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    webfontDownload(
+      [ "https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap&text=0123456789aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ%C4%85%C4%84%C4%87%C4%86%C4%99%C4%98%C5%82%C5%81%C5%84%C5%83%C3%B3%C3%93%C5%9B%C5%9A%C5%BA%C5%B9%C5%BC%C5%B9%21%40%23%24%25%5E%26*%28%29_%2B-%3D%5B%5D%7B%7D%7C%3B%3A%2C.%3C%3E%3F%2F%5C%27%22%7E%60" ],
+      {
+        injectAsStyleTag: true,
+        embedFonts: false,
+      }
+    ),
     Components(components),
     AutoImport(imports),
     getCloudflareBeacon(),
@@ -65,15 +73,15 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          "@vueuse": ["@vueuse/core", "@vueuse/router"],
-          axios: ["axios"],
-          "chroma-js": ["chroma-js"],
-          "string-to-color": ["string-to-color"],
-          vue: ["vue"],
-          "vue-router": ["vue-router"],
-          "floating-vue": ["floating-vue"],
-          "vue-toastification": ["vue-toastification"],
-          sentry: ["@sentry/vue", "@sentry/vite-plugin"],
+          "@vueuse": [ "@vueuse/core", "@vueuse/router" ],
+          axios: [ "axios" ],
+          "chroma-js": [ "chroma-js" ],
+          "string-to-color": [ "string-to-color" ],
+          vue: [ "vue" ],
+          "vue-router": [ "vue-router" ],
+          "floating-vue": [ "floating-vue" ],
+          "vue-toastification": [ "vue-toastification" ],
+          sentry: [ "@sentry/vue", "@sentry/vite-plugin" ],
         },
       },
     },
@@ -87,7 +95,7 @@ export default defineConfig({
       },
     },
     postcss: {
-      plugins: [autoprefixer({})],
+      plugins: [ autoprefixer({}) ],
     },
   },
   resolve: {
