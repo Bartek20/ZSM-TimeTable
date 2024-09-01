d353844cfa3ff0113fb1f38af18860252c564d9c
Bartek20 <67280540+Bartek20@users.noreply.github.com>
2024-09-01 16:24:23 +0200

    Fix colors not reflect change option

--- src/functions/parseLesson.js
+++ src/functions/parseLesson.js
@@ -11,13 +11,13 @@ export default function parseLesson(lesson) {
 	let colors = {};
 
 	// Subject parsing
-	let subjectData = appConfigs.value.timetable.subjects[lesson.subject];
+	let subjectData = appConfigs.value.timetable.subjects[ lesson.subject ];
 	if (lesson.subject.includes('ckz')) {
 		subjectData = appConfigs.value.timetable.subjects.praktyki;
 	}
 	if (subjectData === undefined) {
 		addUnknowns(lesson.subject);
-		subjectData = {short: lesson.subject};
+		subjectData = { short: lesson.subject };
 	}
 	subject = subjectData;
 
@@ -27,7 +27,7 @@ export default function parseLesson(lesson) {
 		columns.left = {
 			name: '@',
 		};
-	} else if (['n', 's'].includes(mode.value)) {
+	} else if ([ 'n', 's' ].includes(mode.value)) {
 		columns.left = {
 			mode: 'o',
 			id: lesson.classId,
@@ -46,7 +46,7 @@ export default function parseLesson(lesson) {
 		columns.right = {
 			name: 'CKZ',
 		};
-	} else if (['o', 'n'].includes(mode.value)) {
+	} else if ([ 'o', 'n' ].includes(mode.value)) {
 		columns.right = {
 			mode: 's',
 			id: appConfigs.value.school.allowStrudentsViewRooms || user.value === 'nauczyciel' ? lesson.roomId : undefined,
@@ -60,17 +60,17 @@ export default function parseLesson(lesson) {
 		};
 	}
 
-	// Colors parsing
-	if (!appConfigs.value.user.showColors) {
-		colors = {
-			light: 'white',
-			dark: 'lightgray',
-		};
-	} else {
-		const name = subject.short.replace(/ \([UR]{1}\)/, '');
-		if (!appConfigs.value.database.subjects[name]) parseColor(name);
-		colors = appConfigs.value.database.subjects[name];
-	}
+	// // Colors parsing
+	// if (!appConfigs.value.user.showColors) {
+	// 	colors = {
+	// 		light: 'white',
+	// 		dark: 'lightgray',
+	// 	};
+	// } else {
+	const name = subject.short.replace(/ \([UR]{1}\)/, '');
+	if (!appConfigs.value.database.subjects[ name ]) parseColor(name);
+	colors = appConfigs.value.database.subjects[ name ];
+	// }
 
 	// Return
 	return {
